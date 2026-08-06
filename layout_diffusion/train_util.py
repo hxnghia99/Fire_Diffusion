913eaf635817d3c6930cb44dc3b2aab9fd375d27
ChrisClaw <hxnghia99@gmail.com>
2026-08-06 06:02:44 +0000

    minor change

--- layout_diffusion/train_util.py
+++ layout_diffusion/train_util.py
@@ -243,9 +243,11 @@ class TrainLoop:
             if self.step % self.log_interval == 0:
                 logger.dumpkvs()
 
+            if self.step % (self.save_interval//10) == 0:
+                self.save_testing_images(self.step, batch, cond)
+
             if self.step % self.save_interval == 0 and self.step > 0:
                 self.save()
-                self.save_testing_images(self.step, batch, cond)
                 
                 # Run for a finite amount of time in integration tests.
                 if os.environ.get("DIFFUSION_TRAINING_TEST", "") and self.step > 0:
@@ -387,13 +389,13 @@ class TrainLoop:
         nir_pred_data = rgbnir_pred_data[0]['sample'][:,3:4]
         fake_nir_fire_img = np.array(nir_pred_data[0].cpu().permute(1,2,0) * 127.5 + 127.5, dtype=np.uint8)
 
-        seg_hard_mask = np.array(rgbnir_pred_data[0]['seg_hard_mask'][0].cpu().permute(1,2,0) * 255, dtype=np.uint8)
+        seg_hard_mask = np.array((1-rgbnir_pred_data[0]['seg_hard_mask'][0].cpu().permute(1,2,0)) * 255, dtype=np.uint8)
         seg_hard_mask = np.repeat(seg_hard_mask, repeats=3, axis=2)
 
         cv2.imwrite(self.img_dir + "/{}_fake_rgb_fire_img.png".format(step),fake_rgb_fire_img)# cv2.cvtColor(fake_rgb_fire_img, cv2.COLOR_RGB2BGR))
         cv2.imwrite(self.img_dir + "/{}_fake_nir_fire_img.png".format(step),fake_nir_fire_img)# cv2.cvtColor(fake_nir_fire_img, cv2.COLOR_RGB2BGR))
         cv2.imwrite(self.img_dir + "/{}_seg_mask.png".format(step),seg_hard_mask)
-        
+
         real_rgb_image = np.array(batch[0,0:3].cpu().permute(1,2,0) * 127.5 + 127.5, dtype=np.uint8)
         real_nir_image = np.array(nir_images.cpu().permute(1,2,0) * 127.5 + 127.5, dtype=np.uint8)
         cv2.imwrite(self.img_dir + "/{}_real_rgb_img.png".format(step),real_rgb_image)# cv2.cvtColor(real_rgb_image, cv2.COLOR_RGB2BGR))
